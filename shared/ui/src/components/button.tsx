a299be3c4004e7a497beff4ee23532af50bb6150
DangTinh42 <caodangtinh04022003@gmail.com>
2025-12-21 17:50:38 +0700

    fix: button style

--- shared/ui/src/components/button.tsx
+++ shared/ui/src/components/button.tsx
@@ -9,8 +9,8 @@ const buttonVariants = cva(
     focus-visible:ring-[3px]
     aria-invalid:ring-destructive/20 aria-invalid:border-destructive
     dark:aria-invalid:ring-destructive/40
-    inline-flex shrink-0 items-center justify-center gap-2 rounded-md text-sm
-    font-medium whitespace-nowrap transition-all outline-none
+    inline-flex shrink-0 cursor-pointer items-center justify-center gap-2
+    rounded-md text-sm font-medium whitespace-nowrap transition-all outline-none
     disabled:pointer-events-none disabled:opacity-50
     [&_svg]:pointer-events-none [&_svg]:shrink-0
     [&_svg:not([class*='size-'])]:size-4
@@ -18,13 +18,12 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-primary text-white hover:bg-primary/90",
         destructive:
           "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
           "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
-        secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+        secondary: "bg-secondary text-foreground hover:bg-secondary/80",
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
