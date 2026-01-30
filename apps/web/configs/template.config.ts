0dde7e2182498dcf03761b4b6778f11873893f5f
DangTinh42 <caodangtinh04022003@gmail.com>
2026-01-30 13:56:57 +0700

    refactor: Standardize template key from 'template01' to 'template-01'.

--- apps/web/configs/template.config.ts
+++ apps/web/configs/template.config.ts
@@ -2,7 +2,7 @@ import Template01 from "@/components/templates/template-01";
 import { type TemplateProp } from "@/components/templates/template-wrapper";
 
 export const TemplateKey = {
-  template01: "template01",
+  template01: "template-01",
 } as const;
 
 export const TEMPLATES: Record<
